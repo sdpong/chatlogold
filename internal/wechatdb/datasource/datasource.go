9d8be2d0e974ba78be4dea82fb0ab58b2dbcf061
sdpong <32691547+sdpong@users.noreply.github.com>
2025-10-06 19:31:55 +0800

    Update datasource.go

--- internal/wechatdb/datasource/datasource.go
+++ internal/wechatdb/datasource/datasource.go
@@ -6,11 +6,11 @@ import (
 
 	"github.com/fsnotify/fsnotify"
 
-	"github.com/sjzar/chatlog/internal/errors"
-	"github.com/sjzar/chatlog/internal/model"
-	"github.com/sjzar/chatlog/internal/wechatdb/datasource/darwinv3"
-	v4 "github.com/sjzar/chatlog/internal/wechatdb/datasource/v4"
-	"github.com/sjzar/chatlog/internal/wechatdb/datasource/windowsv3"
+	"github.com/sdpong/chatlog/internal/errors"
+	"github.com/sdpong/chatlog/internal/model"
+	"github.com/sdpong/chatlog/internal/wechatdb/datasource/darwinv3"
+	v4 "github.com/sdpong/chatlog/internal/wechatdb/datasource/v4"
+	"github.com/sdpong/chatlog/internal/wechatdb/datasource/windowsv3"
 )
 
 type DataSource interface {
