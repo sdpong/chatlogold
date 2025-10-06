1856d6f7873c7b1d27f1de709716df9e93c543e4
sdpong <32691547+sdpong@users.noreply.github.com>
2025-10-06 19:26:29 +0800

    Update extractor.go

--- internal/wechat/key/extractor.go
+++ internal/wechat/key/extractor.go
@@ -3,11 +3,11 @@ package key
 import (
 	"context"
 
-	"github.com/sjzar/chatlog/internal/errors"
-	"github.com/sjzar/chatlog/internal/wechat/decrypt"
-	"github.com/sjzar/chatlog/internal/wechat/key/darwin"
-	"github.com/sjzar/chatlog/internal/wechat/key/windows"
-	"github.com/sjzar/chatlog/internal/wechat/model"
+	"github.com/sdpong/chatlog/internal/errors"
+	"github.com/sdpong/chatlog/internal/wechat/decrypt"
+	"github.com/sdpong/chatlog/internal/wechat/key/darwin"
+	"github.com/sdpong/chatlog/internal/wechat/key/windows"
+	"github.com/sdpong/chatlog/internal/wechat/model"
 )
 
 // Extractor 定义密钥提取器接口
