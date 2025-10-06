7bb06bdf4b34cddfde137e808d948c38decc6f96
sdpong <32691547+sdpong@users.noreply.github.com>
2025-10-07 01:44:05 +0800

    Update detector.go

--- internal/wechat/process/darwin/detector.go
+++ internal/wechat/process/darwin/detector.go
@@ -9,9 +9,9 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/shirou/gopsutil/v4/process"
 
-	"github.com/sdpong/chatlog/internal/errors"
-	"github.com/sdpong/chatlog/internal/wechat/model"
-	"github.com/sdpong/chatlog/pkg/appver"
+	"github.com/sdpong/chatlogold/internal/errors"
+	"github.com/sdpong/chatlogold/internal/wechat/model"
+	"github.com/sdpong/chatlogold/pkg/appver"
 )
 
 const (
