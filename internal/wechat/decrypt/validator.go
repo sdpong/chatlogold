e0a95b00cae6931ad3e7733fd583598c2119487c
sdpong <32691547+sdpong@users.noreply.github.com>
2025-10-06 19:22:03 +0800

    Update validator.go

--- internal/wechat/decrypt/validator.go
+++ internal/wechat/decrypt/validator.go
@@ -3,8 +3,8 @@ package decrypt
 import (
 	"path/filepath"
 
-	"github.com/sjzar/chatlog/internal/wechat/decrypt/common"
-	"github.com/sjzar/chatlog/pkg/util/dat2img"
+	"github.com/sdpong/chatlog/internal/wechat/decrypt/common"
+	"github.com/sdpong/chatlog/pkg/util/dat2img"
 )
 
 type Validator struct {
