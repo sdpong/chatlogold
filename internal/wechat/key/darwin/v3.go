dae367768c3869f31d7d3c3ae658f566e600caba
sdpong <32691547+sdpong@users.noreply.github.com>
2025-10-07 01:40:02 +0800

    Update v3.go

--- internal/wechat/key/darwin/v3.go
+++ internal/wechat/key/darwin/v3.go
@@ -9,10 +9,10 @@ import (
 
 	"github.com/rs/zerolog/log"
 
-	"github.com/sdpong/chatlog/internal/errors"
-	"github.com/sdpong/chatlog/internal/wechat/decrypt"
-	"github.com/sdpong/chatlog/internal/wechat/key/darwin/glance"
-	"github.com/sdpong/chatlog/internal/wechat/model"
+	"github.com/sdpong/chatlogold/internal/errors"
+	"github.com/sdpong/chatlogold/internal/wechat/decrypt"
+	"github.com/sdpong/chatlogold/internal/wechat/key/darwin/glance"
+	"github.com/sdpong/chatlogold/internal/wechat/model"
 )
 
 const (
