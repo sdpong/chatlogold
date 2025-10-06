558f238152870548802f1b792ec2ba1c22d43cfc
sdpong <32691547+sdpong@users.noreply.github.com>
2025-10-06 19:31:14 +0800

    Update datasource.go

--- internal/wechatdb/datasource/v4/datasource.go
+++ internal/wechatdb/datasource/v4/datasource.go
@@ -15,10 +15,10 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/rs/zerolog/log"
 
-	"github.com/sjzar/chatlog/internal/errors"
-	"github.com/sjzar/chatlog/internal/model"
-	"github.com/sjzar/chatlog/internal/wechatdb/datasource/dbm"
-	"github.com/sjzar/chatlog/pkg/util"
+	"github.com/sdpong/chatlog/internal/errors"
+	"github.com/sdpong/chatlog/internal/model"
+	"github.com/sdpong/chatlog/internal/wechatdb/datasource/dbm"
+	"github.com/sdpong/chatlog/pkg/util"
 )
 
 const (
